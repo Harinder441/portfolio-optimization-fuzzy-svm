6dae904ae108986143ec9e6b107b07186f90a671
HarinderBigPC <harindersingh2107@gmail.com>
2022-08-10 11:28:37 +0530

    10 aug

--- PortfolioOptimisation/NewRolling.py
+++ PortfolioOptimisation/NewRolling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from Tuning import *
 from pandas import read_excel
+from openpyxl import load_workbook
 def oneOrMinus(L):  # return -1 or 1 whichever is present max no. of time in List
     ones=0
     minus=0
@@ -23,7 +24,7 @@ df=read_excel(r"C:\Users\Ruchika\Downloads\NIFTY50 2015-Present ALL.xlsx")
 #sdf=df.iloc[:523,3:]
 sdf=df.iloc[1:1861,3:]
 
-selectdf=pd.DataFrame()
+selecteddataframes=[]
 ind=["Trade High","Trade Low","Trade Close","Trade Volume"]
 #get data of each asset
 select=[]
@@ -32,29 +33,36 @@ W=28 # no. of rolling
 #D=50
 D=180 #no. of training days on which rolling performed
 #for i in range(0,20,4):
-for i in range(0,192,4):
-#for i in range(16, 200, 4):
-    df=np.array(sdf.iloc[:,i+1:i+5])
-    df=pd.DataFrame(df,columns=ind)
-    selectrow=[]
-    for j in range(W):
-        dfw=df[j:D+j]
-        r=Tuningandselecting(dfw,Target=0.002)[0]
+Add=0 # I think for SSD
+for j in range(W):
+    selectdf = pd.DataFrame()
+    selectrow = []
+    for i in range(0,192, 4):
+        df = np.array(sdf.iloc[:, i + 1:i + 5])
+        df = pd.DataFrame(df, columns=ind)
+        dfw = df[j+Add:D + j+Add]
+        r = Tuningandselecting(dfw, Target=0.002)[0]
         selectrow.append(r)
+        if r== 1:
+            selectdf[str(int(i / 4))] = sdf.iloc[j+Add:D + j+Add, i + 3:i + 4]
+    # print(selectrow,selectdf)
+    select.append(selectrow)
+    selecteddataframes.append(selectdf)
+    Add+=60
 
-    print(selectrow)
-    maxP=oneOrMinus(selectrow) #decide selected or not
-    print(maxP)
-    select.append(maxP)
-    if maxP==1:
-        selectdf[str(int(i/4))] = sdf.iloc[:,i+3:i+4]
-    #print(select[int((i-16)/4)])
+
+
+
+
 print(select)
-df2 = pd.DataFrame(select)
-print(df2)
-print(selectdf)
-df2.to_excel("shortlisted.xlsx", sheet_name='shortlist')
-df2.to_excel("shortlisted_closingprices.xlsx", sheet_name='1')
+# df2 = pd.DataFrame(select)
+# print(df2)
+writein=pd.ExcelWriter("AfterRolling4.xlsx",engine='xlsxwriter')
+for i in range(len(selecteddataframes)):
+    selecteddataframes[i].to_excel(writein, sheet_name='Rolling'+str(i))
+writein.save()
+# writein.close()
+# df2.to_excel("shortlisted_closingprices.xlsx", sheet_name='1')
 # name=[]
 # name_modified=[]
 # for i in range(len(select)):
