6e575753e16beb437dfc29648583505d3f5b285c
HarinderBigPC <harindersingh2107@gmail.com>
2022-08-11 17:40:42 +0530

    10 aug 0to9rollingSVM

--- PortfolioOptimisation/NewRolling.py
+++ PortfolioOptimisation/NewRolling.py
@@ -35,7 +35,7 @@ D=180 #no. of training days on which rolling performed
 #for i in range(0,20,4):
 Add=60 # I think for SSD
 
-for j in range(10,27):
+for j in range(0,9):
     selectdf = pd.DataFrame()
     selectrow = []
     for i in range(0,192,4):
@@ -53,7 +53,7 @@ for j in range(10,27):
 print(select)
 # df2 = pd.DataFrame(select)
 # print(df2)
-writein=pd.ExcelWriter("RollingResults10to27.xlsx", engine='xlsxwriter')
+writein=pd.ExcelWriter("SVMRollingResults0to9.xlsx", engine='xlsxwriter')
 for i in range(len(selecteddataframes)):
     selecteddataframes[i].to_excel(writein, sheet_name='Rolling'+str(i))
 writein.save()
--- PortfolioOptimisation/Tuning.py
+++ PortfolioOptimisation/Tuning.py
@@ -96,18 +96,22 @@ def Tuningandselecting(df,Target=0.005):
     for i in C:
         for j in K:
             for fs in FSigma:
-                #FS=SVM(dat,labels,C=i,Kernel='R',K_Var=j,Fsigma=None)
-                FS=SVM(dat,labels,C=i,Kernel='R',K_Var=j,Fsigma=fs)
+                FS=SVM(dat,labels,C=i,Kernel='R',K_Var=j,Fsigma=None)
+                #FS=SVM(dat,labels,C=i,Kernel='R',K_Var=j,Fsigma=fs)
                 FS.optimize_alpha()
-                Ker=FS.Kfold(3)
+                try:
+                    Ker=FS.Kfold(3)
+                except:
+                    print("Zero division Error occured in K fold")
+                    return [0]
                 if Ker<E:
                     E=Ker
                     Q[0]=[i,j,fs]
 
     # print(Q,E)
 
-    check=SVM(dat,labels,C=Q[0][0],Kernel='R',K_Var=Q[0][1],Fsigma=Q[0][2],Split_p=100)
-    #check=SVM(dat,labels,C=Q[0][0],Kernel='R',K_Var=Q[0][1],Fsigma=None,Split_p=100)
+    #check=SVM(dat,labels,C=Q[0][0],Kernel='R',K_Var=Q[0][1],Fsigma=Q[0][2],Split_p=100)
+    check=SVM(dat,labels,C=Q[0][0],Kernel='R',K_Var=Q[0][1],Fsigma=None,Split_p=100)
     check.optimize_alpha()
     check.get_b()
     check.GetSupportVector()
