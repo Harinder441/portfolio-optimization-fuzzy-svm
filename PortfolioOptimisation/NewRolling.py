1d928f540503bb9c65d4d988f04f51604a1338e1
Simran <simrandeepkaur107@gmail.com>
2022-08-21 10:57:21 +0530

    nbn

--- PortfolioOptimisation/NewRolling.py
+++ PortfolioOptimisation/NewRolling.py
@@ -20,10 +20,10 @@ def oneOrMinus(L):  # return -1 or 1 whichever is present max no. of time in Lis
 
 
 #df=read_excel(r"C:\Users\Ruchika\PycharmProjects\PortfolioOptimisation\PortfolioOptimisation\Nifty 50 (3).xlsx")
-df=read_excel(r"C:\Users\Simran\Downloads\NIFTY50 2015-Present ALL.xlsx",sheet_name='SVM_FilteredData2015-2022')
+df=read_excel(r"C:\Users\Simran\Documents\Simran Python Data\FTSE100ALL.xlsx",sheet_name='FilteredWithoutIndexAll')
 #sdf=df.iloc[:523,3:]
 sdf=df.iloc[1:1861,3:]
-dfReturns=read_excel(r"C:\Users\Simran\Downloads\NIFTY50 2015-Present ALL.xlsx",sheet_name='SVM_ReturnsAllFeatures')   #give path of return file here
+dfReturns=read_excel(r"C:\Users\Simran\Documents\Simran Python Data\FTSE100ALL.xlsx",sheet_name='ReturnsWithoutIndexAll')   #give path of return file here
 sdfReturns=dfReturns.iloc[1:1861,3:]
 selecteddataframes=[]
 ind=["Trade High","Trade Low","Trade Close","Trade Volume"]
@@ -36,10 +36,10 @@ D=180 #no. of training days on which rolling performed
 #for i in range(0,20,4):
 Add=60 # I think for SSD
 
-for j in range(13,28):
+for j in range(1,2):
     selectdf = pd.DataFrame()
     selectrow = []
-    for i in range(0,192,4):
+    for i in range(0,472,4):
         df = np.array(sdf.iloc[:, i + 1:i + 5])
         df = pd.DataFrame(df, columns=ind)
         dfw = df[j+j*Add:D + j+j*Add]
@@ -54,7 +54,7 @@ for j in range(13,28):
 print(select)
 # df2 = pd.DataFrame(select)
 # print(df2)
-writein=pd.ExcelWriter("FSVMRollingResults13to28.xlsx", engine='xlsxwriter')
+writein=pd.ExcelWriter("FTSE100_FSVMRolling1.xlsx", engine='xlsxwriter')
 for i in range(len(selecteddataframes)):
     selecteddataframes[i].to_excel(writein, sheet_name='Rolling'+str(i))
 writein.save()
