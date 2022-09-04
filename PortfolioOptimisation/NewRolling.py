97a67f3840db950bdfa30bfc62f6e671b7759263
Simran <simrandeepkaur107@gmail.com>
2022-09-04 17:52:20 +0530

    yyu

--- PortfolioOptimisation/NewRolling.py
+++ PortfolioOptimisation/NewRolling.py
@@ -39,7 +39,7 @@ Add=60 # I think for SSD
 for j in range(20,27):
     selectdf = pd.DataFrame()
     selectrow = []
-    for i in range(0,178,4):
+    for i in range(0,188,4):
         df = np.array(sdf.iloc[:, i + 1:i + 5])
         df = pd.DataFrame(df, columns=ind)
         dfw = df[j*Add:D + j*Add]
