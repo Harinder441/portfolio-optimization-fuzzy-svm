c8a6a8625c55380ef1bb7dffee04e37136963044
Simran <simrandeepkaur107@gmail.com>
2022-09-05 23:13:10 +0530

    ggg

--- PortfolioOptimisation/NewRolling.py
+++ PortfolioOptimisation/NewRolling.py
@@ -19,10 +19,12 @@ def oneOrMinus(L):  # return -1 or 1 whichever is present max no. of time in Lis
         return 0
 
 
-#df=read_excel(r"C:\Users\Ruchika\PycharmProjects\PortfolioOptimisation\PortfolioOptimisation\Nifty 50 (3).xlsx")
+#df=read_excel(r"C:\Users\admin\Downloads\NIKKEI400Index.xlsx",sheet_name='FilterAllWithoutIndex')
 df=read_excel(r"C:\Users\admin\Downloads\NIFTY50 2015-Present ALL (1).xlsx",sheet_name='FilteredAllWithoutIndex')
 #sdf=df.iloc[:523,3:]
+#sdf=df.iloc[1317:1801,3:]
 sdf=df.iloc[1:1861,3:]
+#dfReturns=read_excel(r"C:\Users\admin\Downloads\NIKKEI400Index.xlsx",sheet_name='ReturnsAllWithoutIndex')   #give path of return file here
 dfReturns=read_excel(r"C:\Users\admin\Downloads\NIFTY50 2015-Present ALL (1).xlsx",sheet_name='ReturnsWithoutIndex')   #give path of return file here
 sdfReturns=dfReturns.iloc[1:1861,3:]
 selecteddataframes=[]
@@ -31,15 +33,18 @@ ind=["Trade High","Trade Low","Trade Close","Trade Volume"]
 select=[]
 #W=5
 W=27 # no. of rolling
-#D=50
-D=180 #no. of training days on which rolling performed
+D=180
+#D=365 #no. of training days on which rolling performed
 #for i in range(0,20,4):
 Add=60 # I think for SSD
+#Add=120 # I think for SSD
 
-for j in range(12,20):
+for j in range(7,13):
+#for j in range(0,1):
      selectdf = pd.DataFrame()
      selectrow = []
      for i in range(0,188,4):
+     #for i in range(0,1520,4):
          df = np.array(sdf.iloc[:, i + 1:i + 5])
          df = pd.DataFrame(df, columns=ind)
          dfw = df[j*Add:D +j*Add]
@@ -54,7 +59,8 @@ for j in range(12,20):
 print(select)
 # # df2 = pd.DataFrame(select)
 # # print(df2)
-writein=pd.ExcelWriter("NFTY50_FSVMRolling(F).xlsx", engine='xlsxwriter')
+#writein=pd.ExcelWriter("NIKKEI400_FSVMRolling0(F).xlsx", engine='xlsxwriter')
+writein=pd.ExcelWriter("NFTY50_FSVMRolling7to12(F).xlsx", engine='xlsxwriter')
 for i in range(len(selecteddataframes)):
     selecteddataframes[i].to_excel(writein, sheet_name='Rolling'+str(i))
 writein.save()
