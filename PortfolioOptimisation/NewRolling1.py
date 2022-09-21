28683dc88d93787a4825f874540c7b9c65bda95f
hp <simrandeepkaur107@gmail.com>
2022-09-21 09:10:08 +0530

    hjh

--- PortfolioOptimisation/NewRolling1.py
+++ PortfolioOptimisation/NewRolling1.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from Tuning1 import *
+from Tuning import *
 from pandas import read_excel
 from openpyxl import load_workbook
-
 def oneOrMinus(L):  # return -1 or 1 whichever is present max no. of time in List
     ones=0
     minus=0
@@ -18,54 +17,42 @@ def oneOrMinus(L):  # return -1 or 1 whichever is present max no. of time in Lis
     else:
         return 0
 
-#df=read_excel(r"C:\Users\admin\Downloads\NIKKEI400Index.xlsx",sheet_name='FilterAllWithoutIndex')
-df=read_excel(r"C:\Users\Abha\Downloads\LQ 45 Indonesia.xlsx",sheet_name='FilteredAllWithoutIndex')
-#df=read_excel(r"C:\Users\admin\Downloads\S&P 500daily ALL.xlsx",sheet_name='FilteredDataWithoutIndex')
+#df=read_excel(r"C:\Users\Ruchika\PycharmProjects\PortfolioOptimisation\PortfolioOptimisation\Nifty 50 (3).xlsx")
+df=read_excel(r"C:\Users\hp\Downloads\FTSE100ALL.xlsx",sheet_name='FilteredWithoutIndexAll')
 #sdf=df.iloc[:523,3:]
-#sdf=df.iloc[1437:1775,3:]
 sdf=df.iloc[1:1861,3:]
-#dfReturns=read_excel(r"C:\Users\admin\Downloads\NIKKEI400Index.xlsx",sheet_name='ReturnsAllWithoutIndex')   #give path of return file here
-dfReturns=read_excel(r"C:\Users\Abha\Downloads\LQ 45 Indonesia.xlsx",sheet_name='ReturnsAllWithoutIndex')   #give path of return file here
-#dfReturns=read_excel(r"C:\Users\admin\Downloads\S&P 500daily ALL.xlsx",sheet_name='ReturnsAllWithoutIndex')
-#sdfReturns=dfReturns.iloc[1437:1775,3:]
-#print(sdfReturns)
+dfReturns=read_excel(r"C:\Users\hp\Downloads\FTSE100ALL.xlsx",sheet_name='ReturnsWithoutIndexAll')   #give path of return file here
 sdfReturns=dfReturns.iloc[1:1861,3:]
-#print(sdfReturns)
 selecteddataframes=[]
 ind=["Trade High","Trade Low","Trade Close","Trade Volume"]
 #get data of each asset
 select=[]
-W=27 # no. of rolling
-D=180
-#D=365 #no. of training days on which rolling performed
+#W=5
+W=28 # no. of rolling
+#D=50
+D=180 #no. of training days on which rolling performed
 #for i in range(0,20,4):
 Add=60 # I think for SSD
-#Add=120 # I think for SSD
 
-for j in range(0,27):
-#for j in range(0,1):
-     selectdf = pd.DataFrame()
-     selectrow = []
-     #for i in range(0,4,4):
-     for i in range(0,112,4):
-     #for i in range(0,1520,4):
-         df = np.array(sdf.iloc[:, i + 1:i + 5])
-         df = pd.DataFrame(df, columns=ind)
-         dfw = df[j*Add:D +j*Add]
-         r = Tuningandselecting(dfw, Target=0.002)[0]
-         selectrow.append(r)
-         print(r)
-         if r== 1:
-             selectdf[str(int(i / 4))] = sdfReturns.iloc[j*Add:D + j*Add+60, i + 3:i + 4]
-#      print(selectrow,selectdf)
-     select.append(selectrow)
-     selecteddataframes.append(selectdf)
+for j in range(12,14):
+    selectdf = pd.DataFrame()
+    selectrow = []
+    for i in range(0,376,4):
+        df = np.array(sdf.iloc[:, i + 1:i + 5])
+        df = pd.DataFrame(df, columns=ind)
+        dfw = df[j+j*Add:D + j+j*Add]
+        r = Tuningandselecting(dfw, Target=0.002)[0]
+        selectrow.append(r)
+        print(r)
+        if r== 1:
+            selectdf[str(int(i / 4))] = sdfReturns.iloc[j+j*Add:D + j+j*Add+60, i + 3:i + 4]
+    # print(selectrow,selectdf)
+    select.append(selectrow)
+    selecteddataframes.append(selectdf)
 print(select)
-# # df2 = pd.DataFrame(select)
-# # print(df2)
-#writein=pd.ExcelWriter("NIKKEI400_FSVMRolling0(F).xlsx", engine='xlsxwriter')
-writein=pd.ExcelWriter("LQ45_FSVMRolling0to27(F).xlsx", engine='xlsxwriter')
-#writein=pd.ExcelWriter("NIKKEI400_FSVMRolling0(F).xlsx", engine='xlsxwriter')
+# df2 = pd.DataFrame(select)
+# print(df2)
+writein=pd.ExcelWriter("FTSE100SVM_12to13.xlsx", engine='xlsxwriter')
 for i in range(len(selecteddataframes)):
     selecteddataframes[i].to_excel(writein, sheet_name='Rolling'+str(i))
 writein.save()
